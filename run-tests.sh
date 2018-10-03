662367f9176276505dbb23535aeafe6f09cc53bf
Elton Leite <elton.borges@conductor.com.br>
2018-10-03 17:51:00 -0300

    Adiciona Configuração do ModelMapper

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker-compose -f docker-compose.test.yml down
-rm -Rf ./docker/postgres/data-despesa-test
+sudo rm -Rf ./docker/postgres/data-despesa-test
 docker-compose -f docker-compose.test.yml up -d
 mvn clean test
 docker-compose -f docker-compose.test.yml down
